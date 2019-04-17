c86de4714caa6d6e85acdee230540e186025414e
jmanosu <jmanosu@gmail.com>
2019-04-16 17:45:09 -0700

    basic implementation

--- server.js
+++ server.js
@@ -4,26 +4,27 @@ const path = require('path');
 const bodyParser  = require('body-parser');
 const morgan = require('morgan');
 const crypto = require('crypto');
+const dateTime = require('node-datetime');
+const MongoClient = require('mongodb').MongoClient;
+const port = process.env.PORT || 3000;
+var dt = dateTime.create();
+
+var db = null;
+
+var fs = require('fs');
 
 const app = express();
-
-const port = process.env.PORT || 3000;
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, './public/uploads/')
   },
   filename: function (req, file, callback) {
-    console.log(req.body.key);
-    console.log("headers within filename: " + req.headers);
     crypto.pseudoRandomBytes(16, function(err, raw) {
       if (err) return callback(err);
     
@@ -34,43 +35,37 @@ const storage = multer.diskStorage({
 
 var upload = multer({storage: storage});
 
-app.post('/fileUpload',upload.array('file'), (req, res, next) => {
-  //console.log(req);
-  console.log("headers: " + req.headers);
-  
-  //start.js
-  /*
-  var spawn = require('child_process').spawn,
-  py    = spawn('python', ['testChildProcess.py']),
-  data = [1,2,3,4,5,6,7,8,9],
-  dataString = '';
+var url = "mongodb://localhost:27017";
 
-  py.stdout.on('data', function(data){
-  dataString += data.toString();
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+  if (err) throw err;
+  console.log("Database created!");
+  db = client.db('radb');
+  db.collection('Submissions', function (err, collection) {
+    collection.find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
   });
-  py.stdout.on('end', function(){
-  console.log('Sum of numbers=',dataString);
-  });
-  py.stdin.write(JSON.stringify(data));
-  py.stdin.end();
-  */
-  /*
-  var spawn = require('child_process').spawn;
-  var ip    = "52.26.1.56";
-  var port  = 8485;
-  var py    = spawn('python', ['RAPhotoClient.py', ip, port]);
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+});
 
-  py.stdout.on('data', function(data){
-  dataString += data.toString();
-  });
 
-  py.stdout.on('end', function(){
-  console.log('Python');
+app.get('/submissions/photo/:photoURI', (req, res) => {
+  fs.readFile('./public/uploads/'+req.params.photoURI, function (err, content) {
+    if (err) {
+        res.writeHead(400, {'Content-type':'text/html'});
+        res.end("No such image");    
+    } else {
+        //specify the content type in the response will be an image
+        res.writeHead(200,{'Content-type':'image/jpg'});
+        res.end(content);
+    }
   });
-  py.stdin.end();
-  */
+});
 
-  if (!req.files) {
+app.post('/submissions', upload.single('photo'), (req, res) => {
+  if (!req.file) {
     console.log("No file received");
     return res.send({
       success: false
@@ -78,13 +73,45 @@ app.post('/fileUpload',upload.array('file'), (req, res, next) => {
 
   } else {
     console.log('file received');
+    //console.log('userID: ' + req.body.userID + ' jointData ' + req.body.jointData + ' filename: ' + req.file.filename);
+    addSubmission(req.body.userID, req.body.jointData, req.file.filename);
     return res.send({
       success: true
     })
   }
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>hello</h1>');
+app.get('/submissions/:userID', (req, res) => {
+  console.log("userID: " + req.params.userID);
+  db.collection('Submissions', function (err, collection) {
+    if (err) throw err;
+    collection.find({userID: req.params.userID}).toArray(function(err, result) {
+      if (err) throw err;
+      res.send(
+        {
+          "submissions": result
+        }
+      );
+    });
+  });
 });
 
+app.use('*', (req, res, next) => {
+  console.log("The path " + req.originalUrl + " doesn't exist");
+  res.status(404).send({
+    err: "The path " + req.originalUrl + " doesn't exist"
+  });
+});
+
+function addSubmission(userID, jointData, photoURI){
+  if(db == null){
+    return;
+  }
+  db.collection('Submissions', function (err, collection) {
+    if (err) throw err;
+    db.collection('Submissions').countDocuments(function (err, count) {
+      if (err) throw err;
+      collection.insertOne({ id: count, userID: userID,date: dt.format('Y-m-d H:M:S'), status: "Processing", jointData: jointData, photoURI: photoURI });
+    });
+  });
+}
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 import json
 url = 'http://localhost:3000/fileUpload'
-files = {'file': open('./test.png', 'rb')}
+files = {'bob': open('./test.png', 'rb')}
 r = requests.post(url, files=files, data = {'key':'fuck this value'})
 print r.text
